53f2157dbf16a75271c17486953f6b86ddf5e3a3
amaged1896 <ahmed.maged1896@gmail.com>
2022-10-27 22:45:37 +0200

    "side-bar slider done"

---
+++ Yummy/js/main.js
@@ -0,0 +1,12 @@
+// side-bar slider
+
+$("#iconBtn").click(function () {
+  let boxWidth = $(".side-bar").outerWidth();
+  if ($(".side-bar").css("left") == "0px") {
+    $(".side-bar").animate({ left: `-${boxWidth}` }, 500);
+  } else {
+    $(".side-bar").animate({ left: "0px" }, 500);
+  }
+});
+
+// side-bar slider
