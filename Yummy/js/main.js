d810c5cd6ac1b53ededa2bde9a5093af4ebb784e
amaged1896 <ahmed.maged1896@gmail.com>
2022-10-29 22:00:25 +0200

    "display ingredients data"

--- Yummy/js/main.js
+++ Yummy/js/main.js
@@ -1,5 +1,6 @@
 let arrayContainer = [],
-  areaContainer = [];
+  areaContainer = [],
+  ingredientsContainer = [];
 
 let row = document.getElementById("rowData");
 
@@ -58,7 +59,7 @@ function displayCategories() {
   row.innerHTML = box;
 }
 
-// Get Data From API For Area
+// Get Data Area Data From API
 async function getAreaData() {
   apiArea = await fetch(
     "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
@@ -73,7 +74,7 @@ getAreaData().then((apiDataArea) => {
 getAreaData();
 
 // Display Area Data
-console.log(areaContainer);
+// console.log(areaContainer);
 function displayArea() {
   let area = "";
   for (let i = 0; i < areaContainer.length; i++) {
@@ -95,6 +96,45 @@ function displayArea() {
     }
   }
   row.innerHTML = area;
+}
+
+// Get Ingredients Data From API
+async function getIngredientsData() {
+  apiIngredients = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/list.php?i=list"
+  );
+  apiDataIngredients = await apiIngredients.json();
+  console.log(apiDataIngredients);
+  return apiDataIngredients;
+}
+getIngredientsData().then((apiDataIngredients) => {
+  ingredientsContainer.push(apiDataIngredients);
+  displayIngredient();
+});
+getIngredientsData();
+
+// Display ingredients Data
+
+function displayIngredient() {
+  let ingredient = "";
+  for (let i = 0; i < ingredientsContainer.length; i++) {
+    for (let j = 0; j < 20; j++) {
+      ingredient += ` 
+      <div class="col-md-3">
+        <div class="card" style="width: 18rem;">
+          <i class="fa-solid fa-bowl-food fa-3x"></i>
+          <div class="card-body">
+            <h2>Chicken</h2>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the bulk
+              of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>;`;
+    }
+  }
+  row.innerHTML = ingredient;
 }
 
 // Side-bar Slider
