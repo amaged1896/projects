461a85bcc8287fb0628feb1b81bf80b15abd5251
amaged1896 <ahmed.maged1896@gmail.com>
2023-04-07 04:36:34 +0200

    "category controllers"

---
+++ e-commerce/src/modules/category/category.controller.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import { categoryModel } from './../../../databases/models/category.model.js';
+import slug from 'slugify';
+import slugify from "slugify";
+
+const createCategory = async (req, res, next) => {
+    const { name } = req.body;
+
+    let result = new categoryModel({ name, slug: slugify(name) });
+    await result.save();
+
+    res.status(201).json({ message: "success", data: result });
+};
+
+
+
+const getAllCategories = async (req, res, next) => {
+
+    let result = await categoryModel.find();
+    res.status(201).json({ message: "success", data: result });
+};
+
+
+const getCategory = async (req, res, next) => {
+    const { id } = req.params;
+
+    let result = await categoryModel.find(id);
+    res.status(201).json({ message: "success", data: result });
+};
+
+const updateCategory = async (req, res, next) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    let result = await categoryModel.findByIdAndUpdate(id, { name, slug: slugify(name) });
+    res.status(201).json({ message: "success", data: result });
+};
+
+const deleteCategory = async (req, res, next) => {
+    const { id } = req.params;
+    let result = await categoryModel.findByIdAndDelete(id);
+    res.status(201).json({ message: "success", data: result });
+};
+
+export {
+    createCategory,
+    getAllCategories,
+    getCategory,
+    updateCategory,
+    deleteCategory
+};
