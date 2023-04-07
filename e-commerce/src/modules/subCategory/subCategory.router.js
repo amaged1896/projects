7a0cf14fbc2bb9ae6f49a804cbb04385bf081a8d
amaged1896 <ahmed.maged1896@gmail.com>
2023-04-07 07:31:52 +0200

    ''subcategory routes

---
+++ e-commerce/src/modules/subCategory/subCategory.router.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import * as subCategory from './subCategory.controller.js';
+const subCategoryRouter = express.Router();
+
+subCategoryRouter
+    .route('/')
+    .post(subCategory.createSubCategory)
+    .get(subCategory.getAllSubCategories);
+
+subCategoryRouter
+    .route('/:id')
+    .get(subCategory.getSubCategory)
+    .delete(subCategory.deleteSubCategory)
+    .put(subCategory.updateSubCategory);
+
+export default subCategoryRouter;
