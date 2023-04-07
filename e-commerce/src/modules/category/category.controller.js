cfed8958fc70d2c26fe71fdaf7c8b441c6f90130
amaged1896 <ahmed.maged1896@gmail.com>
2023-04-07 06:15:06 +0200

    "Refactoring error handling"

--- e-commerce/src/modules/category/category.controller.js
+++ e-commerce/src/modules/category/category.controller.js
@@ -1,44 +1,49 @@
 import mongoose from "mongoose";
 import { categoryModel } from './../../../databases/models/category.model.js';
-import slug from 'slugify';
 import slugify from "slugify";
 
-const createCategory = async (req, res, next) => {
+
+const catchAsyncError = (fn) => {
+    return (req, res, next) => {
+        fn(req, res, next).catch((error) => {
+            next(error);
+        });
+    };
+};
+
+const createCategory = catchAsyncError(async (req, res, next) => {
     const { name } = req.body;
     let result = new categoryModel({ name, slug: slugify(name) });
     await result.save();
     res.status(201).json({ message: "success", data: result });
-};
+});
 
-
-
-const getAllCategories = async (req, res, next) => {
+const getAllCategories = catchAsyncError(async (req, res, next) => {
     let result = await categoryModel.find();
     res.status(201).json({ message: "success", data: result });
-};
+});
 
-
-const getCategory = async (req, res, next) => {
+const getCategory = catchAsyncError(async (req, res, next) => {
     const { id } = req.params;
     let result = await categoryModel.findById(id);
-    if (!result) return res.status(404).json({ message: "category not found" });
+    if (!result) return next(new AppError(`category not found`, 404));
     res.status(201).json({ message: "success", data: result });
-};
+});
 
-const updateCategory = async (req, res, next) => {
+const updateCategory = catchAsyncError(async (req, res, next) => {
     const { id } = req.params;
     const { name } = req.body;
     let result = await categoryModel.findByIdAndUpdate(id, { name, slug: slugify(name) }, { new: true });
-    if (!result) return res.status(404).json({ message: "category not found" });
+    if (!result) return next(new AppError(`category not found`, 404));
     res.status(201).json({ message: "success", data: result });
-};
+});
 
-const deleteCategory = async (req, res, next) => {
+const deleteCategory = catchAsyncError(async (req, res, next) => {
     const { id } = req.params;
     let result = await categoryModel.findByIdAndDelete(id);
-    if (!result) return res.status(404).json({ message: "category not found" });
+    if (!result) return next(new AppError(`category not found`, 404));
     res.status(201).json({ message: "success", data: result });
-};
+});
 
 export {
     createCategory,
