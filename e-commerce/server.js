ba1a7a69eb2d9f0cfc51277888d35cd6f08f4738
amaged1896 <ahmed.maged1896@gmail.com>
2023-04-07 05:49:05 +0200

    "install morgan"

--- e-commerce/server.js
+++ e-commerce/server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connection } from './databases/connection.js';
 import categoryRouter from './src/modules/category/category.router.js';
+import morgan from 'morgan';
 dotenv.config();
 const app = express();
 const port = 8000;
 
 // middleware
 app.use(express.json());
+app.use(morgan('dev'));
 app.use('/api/v1/categories', categoryRouter);
 app.use('*', (req, res) => res.json({ message: `Can't find this route : ${req.originalUrl}` }));
 
