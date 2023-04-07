8841f1a34bf315a86c23a20d559537b44c3b1180
amaged1896 <ahmed.maged1896@gmail.com>
2023-04-07 06:15:56 +0200

    "global error handling middleware"

--- e-commerce/server.js
+++ e-commerce/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { connection } from './databases/connection.js';
 import categoryRouter from './src/modules/category/category.router.js';
 import morgan from 'morgan';
+import { AppError } from './src/utlis/AppError.js';
 dotenv.config();
 const app = express();
 const port = 8000;
@@ -11,7 +12,14 @@ const port = 8000;
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/v1/categories', categoryRouter);
-app.use('*', (req, res) => res.json({ message: `Can't find this route : ${req.originalUrl}` }));
+app.use('*', (req, res, next) => {
+    next(new AppError(`Can't find this route : ${req.originalUrl}`, 404));
+});
 
+// global error handling middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    res.status(500).json({ error: err.message, statusCode });
+});
 connection();
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
