cc90c7aaf1680501b5ccb0b606fd443d571d2913
amaged1896 <ahmed.maged1896@gmail.com>
2023-04-07 21:18:20 +0200

    ''

--- e-commerce/src/modules/brand/brand.router.js
+++ e-commerce/src/modules/brand/brand.router.js
@@ -5,11 +5,7 @@ const brandRouter = express.Router();
 brandRouter
     .route('/')
     .post(brand.createBrand)
-<<<<<<< HEAD
     .get(brand.getAllBrands);
-=======
-    .get(brand.createBrand);
->>>>>>> 3e5dda55d5f5802e6cd2d11bb6305f11a6b308cf
 
 brandRouter
     .route('/:id')
