028e883f9f22a0a5f5a7d407c9b39a7b60854985
amaged1896 <ahmed.maged1896@gmail.com>
2023-04-07 21:02:24 +0200

    "brand routes"

---
+++ e-commerce/src/modules/brand/brand.router.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import * as brand from './brand.controller.js';
+const brandRouter = express.Router();
+
+brandRouter
+    .route('/')
+    .post('/brand', brand.createBrand)
+    .get('/brand', brand.createBrand);
+
+brandRouter
+    .route(':id')
+    .get(brand.getBrand)
+    .put(brand.updateBrand)
+    .delete(brand.deleteBrand);
+
+export default brandRouter;
