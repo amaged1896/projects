999d9942b5f89735f00d644a88de5f888be495e5
amaged1896 <ahmed.maged1896@gmail.com>
2023-04-07 05:47:21 +0200

    "check if category not found"

--- e-commerce/src/modules/category/category.controller.js
+++ e-commerce/src/modules/category/category.controller.js
@@ -5,17 +5,14 @@ import slugify from "slugify";
 
 const createCategory = async (req, res, next) => {
     const { name } = req.body;
-
     let result = new categoryModel({ name, slug: slugify(name) });
     await result.save();
-
     res.status(201).json({ message: "success", data: result });
 };
 
 
 
 const getAllCategories = async (req, res, next) => {
-
     let result = await categoryModel.find();
     res.status(201).json({ message: "success", data: result });
 };
@@ -23,21 +20,23 @@ const getAllCategories = async (req, res, next) => {
 
 const getCategory = async (req, res, next) => {
     const { id } = req.params;
-
-    let result = await categoryModel.find(id);
+    let result = await categoryModel.findById(id);
+    if (!result) return res.status(404).json({ message: "category not found" });
     res.status(201).json({ message: "success", data: result });
 };
 
 const updateCategory = async (req, res, next) => {
     const { id } = req.params;
     const { name } = req.body;
-    let result = await categoryModel.findByIdAndUpdate(id, { name, slug: slugify(name) });
+    let result = await categoryModel.findByIdAndUpdate(id, { name, slug: slugify(name) }, { new: true });
+    if (!result) return res.status(404).json({ message: "category not found" });
     res.status(201).json({ message: "success", data: result });
 };
 
 const deleteCategory = async (req, res, next) => {
     const { id } = req.params;
     let result = await categoryModel.findByIdAndDelete(id);
+    if (!result) return res.status(404).json({ message: "category not found" });
     res.status(201).json({ message: "success", data: result });
 };
 
