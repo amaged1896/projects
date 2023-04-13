0f435cebff7170ef73d1e6cb4a787826e893d882
amaged1896 <ahmed.maged1896@gmail.com>
2023-04-13 07:41:44 +0200

    "cart"

---
+++ Food App/src/components/Cart/Cart.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import style from './Cart.module.css';
+import Modal from './../UI/Modal';
+
+const Cart = (props) => {
+	const cartItems = (
+		<ul className={style['cart-items']}>
+			{[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+				<li>{item.name}</li>
+			))}
+		</ul>
+	);
+
+	return (
+		<Modal onClose={props.onClose}>
+			{cartItems}
+			<div className={style.total}>
+				<span>Total Amount</span>
+				<span>35.62</span>
+			</div>
+			<div className={style.actions}>
+				<button className={style['button--alt']} onClick={props.onClose}>
+					Close
+				</button>
+				<button className={style.button}>Order</button>
+			</div>
+		</Modal>
+	);
+};
+
+export default Cart;
