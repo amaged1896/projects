160be69a3a80a36c1d3b11c38a73e0fec148eeec
amaged1896 <ahmed.maged1896@gmail.com>
2023-04-13 22:34:19 +0200

    "animate cart button"

--- Food App/src/components/Layout/HeaderCartButton.jsx
+++ Food App/src/components/Layout/HeaderCartButton.jsx
@@ -1,16 +1,32 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import style from './HeaderCartButton.module.css';
 import CartContext from '../../store/cart-context';
 const HeaderCartButton = (props) => {
-
+	const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 	const cartCtx = useContext(CartContext);
-	const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+
+	const { items } = cartCtx;
+	const numberOfCartItems = items.reduce((curNumber, item) => {
 		return curNumber + item.amount;
 	}, 0);
+	const btnClasses = `${style.button} ${btnIsHighlighted ? style.bump : ''}`;
+
+	useEffect(() => {
+		if (items.length === 0) {
+			return;
+		}
+		setBtnIsHighlighted(true);
+
+		const timer = setTimeout(() => {
+			setBtnIsHighlighted(false);
+		}, 300);
+
+		return () => clearTimeout(timer);
+	}, [items]);
 
 	return (
-		<button className={style.button} onClick={props.onShowCart}>
+		<button className={btnClasses} onClick={props.onShowCart}>
 			<span className={style.icon}>
 				<CartIcon />
 			</span>
