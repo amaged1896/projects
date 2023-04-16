cd583bb828cf7406acf3d58fbd5e7bb441ddd3a1
amaged1896 <ahmed.maged1896@gmail.com>
2023-04-16 05:25:31 +0200

    "home page"

--- Yummy/js/index.js
+++ Yummy/js/index.js
@@ -1,9 +1,13 @@
+let rowData = document.getElementById('rowData');
+
 function openSideNav() {
     $(".side-nav-menu").animate({ left: 0 }, 500);
     $(".open-close-icon").removeClass("fa-align-justify");
     $(".open-close-icon").addClass("fa-x");
     for (let i = 0; i < 5; i++) {
-        $(".links li").eq(i).animate({ top: 0 }, (i + 5) * 100);
+        $(".links li")
+            .eq(i)
+            .animate({ top: 0 }, (i + 5) * 100);
     }
 }
 
@@ -25,3 +29,28 @@ $(".side-nav-menu i.open-close-icon").click(() => {
     }
 });
 
+async function searchByName(term) {
+    let response = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`
+    );
+    response = await response.json();
+    console.log(response.meals);
+    displayMeals(response.meals);
+}
+
+function displayMeals(arr) {
+    let box = '';
+    for (let i = 0; i < arr.length; i++) {
+        box += `
+                <div class="col-md-3">
+                    <div class="meal position-relative overflow-hidden rounded-2">
+                        <img class="w-100" src="${arr[i].strMealThumb}" alt="">
+                        <div class="meal-layer position-absolute d-flex align-items-center p-2">
+                         <h3 class="text-center text-black">${arr[i].strMeal}</h3>
+                        </div> 
+                    </div>
+                </div>`;
+    }
+    rowData.innerHTML = box;
+}
+searchByName("");
