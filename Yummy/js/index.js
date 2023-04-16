14106cd11dc32016ea457b8ccb302f4f07cff366
amaged1896 <ahmed.maged1896@gmail.com>
2023-04-16 06:08:41 +0200

    "display area"

--- Yummy/js/index.js
+++ Yummy/js/index.js
@@ -43,9 +43,9 @@ function displayMeals(arr) {
     for (let i = 0; i < arr.length; i++) {
         box += `
                 <div class="col-md-3">
-                    <div class="meal position-relative overflow-hidden rounded-2">
+                    <div class="meal position-relative overflow-hidden rounded-2 cursor-pointer">
                         <img class="w-100" src="${arr[i].strMealThumb}" alt="">
-                        <div class="meal-layer position-absolute d-flex align-items-center p-2">
+                        <div class="meal-layer position-absolute d-flex justify-content-center align-items-center p-2">
                          <h3 class="text-center text-black">${arr[i].strMeal}</h3>
                         </div> 
                     </div>
@@ -67,12 +67,33 @@ function displayCategories(arr) {
     for (let i = 0; i < arr.length; i++) {
         box += `
                 <div class="col-md-3">
-                    <div class="meal position-relative overflow-hidden rounded-2">
+                    <div class="meal position-relative overflow-hidden rounded-2 cursor-pointer">
                         <img class="w-100" src="${arr[i].strCategoryThumb}" alt="">
                         <div class="meal-layer position-absolute text-center text-black p-2">
                          <h3 class="text-center text-black">${arr[i].strCategory}</h3>
                          <p>${arr[i].strCategoryDescription.split(" ").slice(0, 20).join(" ")}</p>
                         </div> 
+                    </div>
+                </div>`;
+    }
+    rowData.innerHTML = box;
+}
+
+async function getArea() {
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`);
+    response = await response.json();
+    console.log(response);
+    displayArea(response.meals);
+}
+
+function displayArea(arr) {
+    let box = '';
+    for (let i = 0; i < arr.length; i++) {
+        box += `
+                <div class="col-md-3">
+                    <div class="meal text-center rounded-2 cursor-pointer">
+                            <i class="fa-solid fa-house-laptop fa-4x text-center"></i>
+                            <h3 class="text-center text-white">${arr[i].strArea}</h3>
                     </div>
                 </div>`;
     }
