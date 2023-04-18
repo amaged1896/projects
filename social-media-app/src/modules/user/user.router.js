f3d595cd8f4b627b4aed983438e245f18cf83f75
Ahmed Maged <ahmed.maged1896@gmail.com>
2023-04-19 01:48:05 +0200

    Delete social-media-app directory

--- social-media-app/src/modules/user/user.router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as userControllers from './user.controller.js';
-
-const userRouter = express.Router();
-
-userRouter.post('/signup', userControllers.signup);
-userRouter.post('/signin', userControllers.signin);
-
-export default userRouter;
